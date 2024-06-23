675d135f9c75bbd1b82122a609fb51e302c48e84
SATYAJEET323 <160376609+SATYAJEET323@users.noreply.github.com>
2024-06-23 13:10:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new Typed('.text', {
-    strings: ["Web Developer","Full Stack Developer","Coder " ,"DataBase Engineer" , "Graphic Designer" , "3D Artist" , "UI/UX Designer"],
+    strings: ["Web Developer","Coder " ,"DataBase Engineer" , "Graphic Designer" , "3D Artist" , "UI/UX Designer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
@@ -15,3 +15,24 @@ var typed= new Typed('.text', {
       .then(response => console.log('Success!', response))
       .catch(error => console.error('Error!', error.message))
   })
+
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.forms['submit-to-google-sheet'];
+  form.addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent form from submitting and redirecting
+
+      const name = form['Name'].value.trim();
+      const email = form['Email'].value.trim();
+      const number = form['Number'].value.trim();
+      const subject = form['Subject'].value.trim();
+      const message = form['Message'].value.trim();
+      
+      if (!name || !email || !number || !subject || !message) {
+          alert("Please fill all spaces.");
+      } else {
+          alert("Thank you! Your message has been sent.");
+          // Optionally, clear the form fields
+          form.reset();
+      }
+  });
+});
